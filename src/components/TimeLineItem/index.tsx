b7e1f09eaa21f444da77164d8b290604c97d4321
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2025-05-15 20:11:26 -0300

    feat: adiciona verificação se o item da linha do tempo está pressionado

--- src/components/TimeLineItem/index.tsx
+++ src/components/TimeLineItem/index.tsx
@@ -1,20 +1,22 @@
 import { ImageSquare } from "@phosphor-icons/react"
 import style from './style.module.css'
+import type { SelectedEventType, TimeLineItemType } from "../../types"
 
-type Items = {
-  status: string, 
-  date: string
-}
 
 type TimeLineItemProps = {
-  item: Items
+  item: TimeLineItemType
+  handleEventTimeLineItemClick: (eventItem: TimeLineItemType) => void
+  selectedEvents: SelectedEventType
 }
-function TimeLineItem({ item }: TimeLineItemProps) {
+function TimeLineItem({ item, handleEventTimeLineItemClick, selectedEvents }: TimeLineItemProps) {
+
+  const isSelected = selectedEvents.eventOne?.id === item.id || selectedEvents.eventTwo?.id === item.id;
+
   return (
     <div className={style.item__container}>
-      <p className={style.timeline_item__status}>{item.status}</p>
+      <p className={style.timeline_item__status}>{item.title}</p>
       <p className={style.timeline_item__date}>{item.date}</p>
-      <div className={style.timeline_item__icon}>
+      <div className={isSelected ? style.timeline_item__icon__pressed : style.timeline_item__icon} onClick={() => handleEventTimeLineItemClick(item)}>
         <ImageSquare size={32} color="#fbeb0b" />
       </div>
     </div>
