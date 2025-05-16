9eb2242f8e708b90d49ab07a028398b25fa0b4c6
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2025-05-16 00:43:16 -0300

    feat: substitui título por logotipo no cabeçalho e ajusta estilos da imagem

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
 import HeaderNav from '../HeaderNav'
 import style from './style.module.css'
 import HeaderUser from '../HeaderUser'
+import logo from '../../assets/logo.svg'
 
 function Header() {
   return (
     <header className={style.header__wrapper}>
       <div className={style.menu__wrapper_left_side}>
-        <h1>My App</h1>
+        <img src={logo} alt="logotipo fiscaliza" />
         <HeaderNav />
       </div>
       <div  className={style.menu__wrapper_right_side}>
