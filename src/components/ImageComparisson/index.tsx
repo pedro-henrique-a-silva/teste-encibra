3b3d59b3d639c25fd530c46b3abd03ae73d4eb81
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2025-05-15 21:58:41 -0300

    feat: adicionado o titulo da seção do slider
    - Adicionado icone de imagem do lado esquerdo do titulo

--- src/components/ImageComparisson/index.tsx
+++ src/components/ImageComparisson/index.tsx
@@ -1,6 +1,7 @@
 import style from './style.module.css'
 import { ReactCompareSlider, ReactCompareSliderImage, styleFitContainer } from 'react-compare-slider';
 import type { SelectedEventType } from '../../types';
+import { ImageSquare } from '@phosphor-icons/react';
 
 type ImageComparissonProps = {
   selectedEvents: SelectedEventType
@@ -9,6 +10,10 @@ type ImageComparissonProps = {
 function ImageComparisson({ selectedEvents }: ImageComparissonProps) {
   return (
     <div className={style.image_slider__container}>
+      <h2 className={style.image_slider__title}>
+        <ImageSquare size={32} color="#1f1f1f" weight="fill" />
+        Slider
+      </h2>
       {(selectedEvents.eventOne || selectedEvents.eventTwo) && (
         <ReactCompareSlider
           itemOne={<ReactCompareSliderImage src={selectedEvents.eventOne?.imageUrl} alt="Image one" />}
