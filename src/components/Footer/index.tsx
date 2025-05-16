a5cdb92b7b5757a1608553824a09ec498186941f
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2025-05-16 09:05:43 -0300

    feat: adiciona componente Footer com estilos e logo

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,27 @@
+import style from './style.module.css'
+import logoFooter from '../../assets/logo-footer.svg'
+
+function Footer() {
+  return (
+    <footer className={style.footer__wrapper}>
+      <div>
+        <div>
+          <img src={logoFooter} alt="Logo Footer" className={style.footer__logo} />
+          <p className={style.footer_encibra__name}>
+            Encibra
+          </p>
+        </div>
+        <span className={style.footer_copy__right}>
+          <strong>
+            Fiscaliza
+          </strong>
+          <span></span>
+          Encibra 2025 Todos os direitos reservados
+        </span>
+
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
